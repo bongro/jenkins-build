7d4730585779f82b3ca882c5d8a10b9218e33b38
bongro <591175089@qq.com>
2017-12-09 20:47:53 +0800

    提交人：bongor 提交内容：随便改改

--- androidProject/git/jenkins-build/app/src/main/java/com/jenkins/bongor/jenkinsbuild/MainActivity.java
+++ androidProject/git/jenkins-build/app/src/main/java/com/jenkins/bongor/jenkinsbuild/MainActivity.java
@@ -1,9 +1,19 @@
 package com.jenkins.bongor.jenkinsbuild;
 
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +22,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ApplicationInfo appInfo = null;
+        try {
+            appInfo = this.getPackageManager()
+                    .getApplicationInfo(getPackageName(),
+                            PackageManager.GET_META_DATA);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        final int msg = appInfo.metaData.getInt("channel");
+
         findViewById(R.id.tv).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Congratulations!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, String.valueOf(msg), Toast.LENGTH_SHORT).show();
             }
         });
     }
